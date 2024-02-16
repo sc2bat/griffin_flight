c0a8f8c436503620e001818f68728e3d6c237cf4
sc2bat <izar_dero@naver.com>
2024-02-16 11:33:44 +0900

    Feat: update 세팅

--- lib/data/http.dart
+++ lib/data/http.dart
@@ -1,16 +1,16 @@
-import 'package:griffin/utils/simple_logger.dart';
-import 'package:http/http.dart' as http;
-import 'package:http/http.dart';
+// import 'package:griffin/utils/simple_logger.dart';
+// import 'package:http/http.dart' as http;
+// import 'package:http/http.dart';
 
-Future<Response> fetchHttp(String url) async {
-  try {
-    final response = await http.get(Uri.parse(url));
+// Future<Response> fetchHttp(String url) async {
+//   try {
+//     final response = await http.get(Uri.parse(url));
 
-    if (response.statusCode == 200) {
-      return response;
-    }
-  } catch (e) {
-    logger.info('http 통신 에러 => $e');
-    throw Exception(e);
-  }
-}
+//     if (response.statusCode == 200) {
+//       return response;
+//     }
+//   } catch (e) {
+//     logger.info('http 통신 에러 => $e');
+//     throw Exception(e);
+//   }
+// }
