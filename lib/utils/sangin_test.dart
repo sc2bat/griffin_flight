1887b74432a23ba747d12cb7ef13b872d621cebd
Sangin Han <hugedreamhd@gmail.com>
2024-02-16 14:07:54 +0900

    feat: test 커밋

---
+++ lib/utils/sangin_test.dart
@@ -0,0 +1,10 @@
+import 'package:flutter/cupertino.dart';
+
+class StringName extends StatelessWidget {
+  const StringName({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Container();
+  }
+}
