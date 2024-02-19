93f57cef2d7fe5ab3f5e747fab5c8866a2eeda7e
eunjinjeong <eunjin.jeong.5115@gmail.com>
2024-02-19 16:31:31 +0900

    feat: usecase 폴더명 변경

---
+++ lib/domain/use_cases/reservation/send_results_use_case.dart
@@ -0,0 +1,2 @@
+class SendResultsUseCase {
+}
---
+++ lib/presentation/reservation/flight_results.dart
@@ -0,0 +1,10 @@
+import 'package:flutter/material.dart';
+
+class FlightResults extends StatelessWidget {
+  const FlightResults({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return const Placeholder();
+  }
+}
