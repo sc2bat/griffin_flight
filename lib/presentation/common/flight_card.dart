b8d32d9c4b40aee885ed9f6e69a407f7ebfd1126
eunjinjeong <eunjin.jeong.5115@gmail.com>
2024-02-22 15:54:42 +0900

    feat: flight card UI 틀작업

--- lib/presentation/common/flight_card.dart
+++ lib/presentation/common/flight_card.dart
@@ -2,7 +2,12 @@ import 'package:flutter/material.dart';
 import 'package:griffin/presentation/common/colors.dart';
 
 class FlightDetailsCard extends StatelessWidget {
-  const FlightDetailsCard({super.key, required this.width, required this.height, required this.title});
+  const FlightDetailsCard(
+      {super.key,
+      required this.width,
+      required this.height,
+      required this.title});
+
   final double width;
   final double height;
   final String title;
@@ -10,12 +15,40 @@ class FlightDetailsCard extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Card(
-      color: AppColors.greyCard,
-      child: Container(
-        height: height,
-        width: width,
-        child: Text(title)
-      ),
-    );
+        color: AppColors.greyCard,
+        child: Container(
+          height: height,
+          width: width,
+          child: const Padding(
+            padding: EdgeInsets.all(16.0),
+            child: Column(
+                crossAxisAlignment: CrossAxisAlignment.center,
+                children: [
+                  Row(
+                    children: [
+                      Text('to-from'),
+                      Spacer(),
+                      Text('특징'),
+                    ],
+                  ),
+                  Expanded(
+                    child: ListTile(
+                      contentPadding: EdgeInsets.zero,
+                      title: Text('시간'),
+                      subtitle: Text('소요시간'),
+                      trailing: Text(
+                        '금액',
+                        style: TextStyle(fontSize: 28),
+                      ),
+                    ),
+                  ),
+                  Row(
+                    children: [
+                      Text('항공편 정보'),
+                    ],
+                  ),
+                ]),
+          ),
+        ));
   }
 }
