defd9bee9006cdd6b844011e178e94110ece4120
Sangin Han <hugedreamhd@gmail.com>
2024-02-22 18:30:55 +0900

    fix: 쓸떼없는 주석 제거

--- lib/data/data_sources/apis/sample_api.dart
+++ lib/data/data_sources/apis/sample_api.dart
@@ -28,29 +28,3 @@ class SampleApi {
     );
   }
 }
-
-// from을 누를 때 마다
-// AppManager 싱글턴 클래스
-/*
-Class AppManager {
-  List<AirportDTO> airportDTOModels = <AirportDTO>[];
-
-  getAirport() async {
-    airportDTOModels.claer();
-    airportDTOModels = await SampleApi.getSampleAirportFromGit();
-
-    for (int i = 0; i <= airportDTOModels.length; i++) {
-      airportDTOModels[i].
-    }
-  }
-
-  getAirport(); // 데이터를 받아오기.
-  // 데이터를 받아오면 정보가 airportDTOModels에 들어가있어요.
-}*/
-
-
-// 0. sliding up panel이 올라가는 순간 또는 검색을 하는 순간
-// InkWell
-// 검색을 하라고 누른 순간
-// 1. getSampleAirportFromGit() 목업 데이터를 받아와서 "내부에 저장"
-// 2. 저장된 데이터를 가지고
